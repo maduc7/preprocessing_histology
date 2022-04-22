8734179a3c535fc23b01929283e289363eb144ec
maduc7 <maduc7@gmail.com>
2022-04-22 14:35:47 +0200

    filter pen marks + save grayscale image

--- src/utils.py
+++ src/utils.py
@@ -152,6 +152,24 @@ def save_pil_img(pil_img: Image,
     pil_img.save(path_save, ext)
 
 
+def save_pil_gray_img(pil_img: Image,
+                      path_save: str,
+                      ext: str = "tiff",
+                      verbose: bool = False
+                      ) -> None:
+    """
+    Save image as PIL
+
+    :param pil_img: PIL image
+    :param path_save: path to the image to save
+    :param ext: extension of the image
+    :param verbose
+    :return: None
+    """
+    if verbose: print("Save image as PIL image: {}".format(path_save))
+    pil_img.convert("L").save(path_save, ext)
+
+
 def pil_to_np(pil_img: Image,
               verbose: bool = False
               ) -> np.ndarray:
@@ -178,25 +196,6 @@ def np_to_pil(np_img: np.ndarray,
     """
     if verbose: print("Convert numpy array to PIL image")
     return Image.fromarray(np_img)
-
-
-def save_pil_image(pil_img: Image,
-                   path_save: str,
-                   extension_save: str = 'tiff',
-                   verbose: bool = False
-                   ) -> None:
-    """
-    save PIL image
-    :param pil_img: pil image
-    :param path_save: path to the image to save
-    :param extension_save: extension of the image
-    :param verbose:
-    :return: None
-    """
-    if verbose:
-        print("Saving PIL image as: ", path_save+"."+extension_save)
-
-    pil_img.save(path_save, extension_save)
 
 
 def np_rgb_to_np_gray(rgb_np: np.ndarray,
@@ -308,3 +307,160 @@ def mask_to_img(np_img: np.ndarray,
     """
     if verbose: print("Masking applied on numpy rgb image.")
     return np_img * mask[..., None]
+
+
+def filter_red(np_img: np.ndarray,
+               red_lower_thresh: int,
+               green_upper_thresh: int,
+               blue_upper_thresh: int
+               ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 771 to 802 (modified)
+    """
+    Create a mask to filter out reddish colors, where the mask is based on a pixel being above a
+    red channel threshold value, below a green channel threshold value, and below a blue channel threshold value.
+    :param np_img: GB image as a NumPy array.
+    :param red_lower_thresh: Red channel lower threshold value.
+    :param green_upper_thresh: Green channel upper threshold value.
+    :param blue_upper_thresh: Blue channel upper threshold value.
+    :return: NumPy array representing the mask.
+    """
+    r = np_img[:, :, 0] > red_lower_thresh
+    g = np_img[:, :, 1] < green_upper_thresh
+    b = np_img[:, :, 2] < blue_upper_thresh
+    return ~(r & g & b)
+
+
+def filter_red_pen(np_img: np.ndarray
+                   ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 805 to 833
+    """
+    Create a mask to filter out red pen marks from a slide.
+    :param np_img: RGB image as a NumPy array.
+    :return: NumPy array representing the mask.
+    """
+    result = filter_red(np_img, red_lower_thresh=150, green_upper_thresh=80, blue_upper_thresh=90) & \
+             filter_red(np_img, red_lower_thresh=110, green_upper_thresh=20, blue_upper_thresh=30) & \
+             filter_red(np_img, red_lower_thresh=185, green_upper_thresh=65, blue_upper_thresh=105) & \
+             filter_red(np_img, red_lower_thresh=195, green_upper_thresh=85, blue_upper_thresh=125) & \
+             filter_red(np_img, red_lower_thresh=220, green_upper_thresh=115, blue_upper_thresh=145) & \
+             filter_red(np_img, red_lower_thresh=125, green_upper_thresh=40, blue_upper_thresh=70) & \
+             filter_red(np_img, red_lower_thresh=200, green_upper_thresh=120, blue_upper_thresh=150) & \
+             filter_red(np_img, red_lower_thresh=100, green_upper_thresh=50, blue_upper_thresh=65) & \
+             filter_red(np_img, red_lower_thresh=85, green_upper_thresh=25, blue_upper_thresh=45)
+    return result
+
+
+def filter_green(np_img: np.ndarray,
+                 red_upper_thresh: int,
+                 green_lower_thresh: int,
+                 blue_lower_thresh: int,
+                 ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 836 to 869
+    """
+
+    :param np_img: RGB image as a NumPy array.
+    :param red_upper_thresh: Red channel upper threshold value.
+    :param green_lower_thresh: Green channel lower threshold value.
+    :param blue_lower_thresh: Blue channel lower threshold value.
+    :return: NumPy array representing the mask.
+    """
+    r = np_img[:, :, 0] < red_upper_thresh
+    g = np_img[:, :, 1] > green_lower_thresh
+    b = np_img[:, :, 2] > blue_lower_thresh
+    return ~(r & g & b)
+
+
+def filter_green_pen(np_img: np.ndarray,
+                     ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 872 to 906
+    """
+    Create a mask to filter out green pen marks from a slide.
+    :param np_img: RGB image as a NumPy array.
+    :return: NumPy array representing the mask.
+    """
+    result = filter_green(np_img, red_upper_thresh=150, green_lower_thresh=160, blue_lower_thresh=140) & \
+             filter_green(np_img, red_upper_thresh=70, green_lower_thresh=110, blue_lower_thresh=110) & \
+             filter_green(np_img, red_upper_thresh=45, green_lower_thresh=115, blue_lower_thresh=100) & \
+             filter_green(np_img, red_upper_thresh=30, green_lower_thresh=75, blue_lower_thresh=60) & \
+             filter_green(np_img, red_upper_thresh=195, green_lower_thresh=220, blue_lower_thresh=210) & \
+             filter_green(np_img, red_upper_thresh=225, green_lower_thresh=230, blue_lower_thresh=225) & \
+             filter_green(np_img, red_upper_thresh=170, green_lower_thresh=210, blue_lower_thresh=200) & \
+             filter_green(np_img, red_upper_thresh=20, green_lower_thresh=30, blue_lower_thresh=20) & \
+             filter_green(np_img, red_upper_thresh=50, green_lower_thresh=60, blue_lower_thresh=40) & \
+             filter_green(np_img, red_upper_thresh=30, green_lower_thresh=50, blue_lower_thresh=35) & \
+             filter_green(np_img, red_upper_thresh=65, green_lower_thresh=70, blue_lower_thresh=60) & \
+             filter_green(np_img, red_upper_thresh=100, green_lower_thresh=110, blue_lower_thresh=105) & \
+             filter_green(np_img, red_upper_thresh=165, green_lower_thresh=180, blue_lower_thresh=180) & \
+             filter_green(np_img, red_upper_thresh=140, green_lower_thresh=140, blue_lower_thresh=150) & \
+             filter_green(np_img, red_upper_thresh=185, green_lower_thresh=195, blue_lower_thresh=195)
+    return result
+
+
+def filter_blue(np_img: np.ndarray,
+                red_upper_thresh: int,
+                green_upper_thresh: int,
+                blue_lower_thresh: int
+                ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 909 to 940
+    """
+    Create a mask to filter out blueish colors, where the mask is based on a pixel being below a
+    red channel threshold value, below a green channel threshold value, and above a blue channel threshold value.
+
+    :param np_img: RGB image as a NumPy array.
+    :param red_upper_thresh: Red channel upper threshold value.
+    :param green_upper_thresh:  Green channel upper threshold value.
+    :param blue_lower_thresh: Blue channel lower threshold value.
+    :return: NumPy array representing the mask.
+    """
+    r = np_img[:, :, 0] < red_upper_thresh
+    g = np_img[:, :, 1] < green_upper_thresh
+    b = np_img[:, :, 2] > blue_lower_thresh
+    return ~(r & g & b)
+
+
+def filter_blue_pen(np_img: np.ndarray
+                    ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 943 to 974
+    """
+    Create a mask to filter out blue pen marks from a slide.
+    :param np_img: RGB image as a NumPy array.
+    :return: NumPy array representing the mask.
+    """
+    result = filter_blue(np_img, red_upper_thresh=60, green_upper_thresh=120, blue_lower_thresh=190) & \
+             filter_blue(np_img, red_upper_thresh=120, green_upper_thresh=170, blue_lower_thresh=200) & \
+             filter_blue(np_img, red_upper_thresh=175, green_upper_thresh=210, blue_lower_thresh=230) & \
+             filter_blue(np_img, red_upper_thresh=145, green_upper_thresh=180, blue_lower_thresh=210) & \
+             filter_blue(np_img, red_upper_thresh=37, green_upper_thresh=95, blue_lower_thresh=160) & \
+             filter_blue(np_img, red_upper_thresh=30, green_upper_thresh=65, blue_lower_thresh=130) & \
+             filter_blue(np_img, red_upper_thresh=130, green_upper_thresh=155, blue_lower_thresh=180) & \
+             filter_blue(np_img, red_upper_thresh=40, green_upper_thresh=35, blue_lower_thresh=85) & \
+             filter_blue(np_img, red_upper_thresh=30, green_upper_thresh=20, blue_lower_thresh=65) & \
+             filter_blue(np_img, red_upper_thresh=90, green_upper_thresh=90, blue_lower_thresh=140) & \
+             filter_blue(np_img, red_upper_thresh=60, green_upper_thresh=60, blue_lower_thresh=120) & \
+             filter_blue(np_img, red_upper_thresh=110, green_upper_thresh=110, blue_lower_thresh=175)
+    return result
+
+
+def filter_grays(np_img: np.ndarray,
+                 tolerance: int = 15
+                 ) -> np.ndarray:
+    # https://github.com/deroneriksson/python-wsi-preprocessing/blob/master/deephistopath/wsi/filter.py
+    # 977 to 1005
+    """
+    Create a mask to filter out pixels where the red, green, and blue channel values are similar.
+    :param np_img: RGB image as a NumPy array.
+    :param tolerance: Tolerance value to determine how similar the values must be in order to be filtered out
+    :return: NumPy array representing a mask where pixels with similar red, green, and blue values have been masked out.
+    """
+    np_img = np_img.astype(np.int)
+    rg_diff = abs(np_img[:, :, 0] - np_img[:, :, 1]) <= tolerance
+    rb_diff = abs(np_img[:, :, 0] - np_img[:, :, 2]) <= tolerance
+    gb_diff = abs(np_img[:, :, 1] - np_img[:, :, 2]) <= tolerance
+
+    return ~(rg_diff & rb_diff & gb_diff)
